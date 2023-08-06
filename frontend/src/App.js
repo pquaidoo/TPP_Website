fea32330d55f1586f140a784346ab7c6024464f1
Lwazi Mabota <greninja71@gmail.com>
2023-08-05 20:34:15 -0700

    Added Mewtwo Game png and card

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import SimpleZoom from "./components/slogan.components";
 import PictureWithTextbox from "./components/map.components";
+import BlueBoy from "./components/project.components";
 
 function App() {
   return <div>
           <SimpleZoom />
           <PictureWithTextbox/>
+          <BlueBoy />
+          
         </div>;
+
   
 }
 
