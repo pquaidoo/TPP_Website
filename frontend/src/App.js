12815993ee9fe5f3d12611413fade6056c5433de
Bill Marban <billym41620@gmail.com>
2023-08-09 15:40:50 -0700

    About Us: Top Section done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,17 @@ import "./App.css";
 import SimpleZoom from "./components/slogan.components";
 import PictureWithTextbox from "./components/map.components";
 import BlueBoy from "./components/project.components";
+import AboutUs from "./components/AboutUs/AboutUs.component";
 
 function App() {
-  return <div>
-          <SimpleZoom />
-          <PictureWithTextbox/>
-          <BlueBoy />
-          
-        </div>;
-
-  
+  return (
+    <div>
+      <SimpleZoom />
+      <PictureWithTextbox />
+      <BlueBoy />
+      <AboutUs />
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/AboutUs/AboutUs.component.jsx
@@ -0,0 +1,12 @@
+import CircleWithImage from "./TopSection.components";
+
+let AboutUs = () => {
+  return (
+    <CircleWithImage
+      imageUrl="https://pbs.twimg.com/profile_images/949787136030539782/LnRrYf6e_400x400.jpg"
+      text="Founder"
+    />
+  );
+};
+
+export default AboutUs;
