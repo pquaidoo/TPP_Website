96afa12d95c42354d5a5c729c4959a7740bf9560
Bill Marban <billym41620@gmail.com>
2023-08-27 20:30:21 -0700

    resolved issues

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,32 +2,33 @@ import "./App.css";
 import SimpleZoom from "./components/slogan.components";
 import PictureWithTextbox from "./components/map.components";
 import BlueBoy from "./components/project.components";
+import Header from "./header";
+import Footer from "./footer";
 
 function App() {
-  return <div>
-          <SimpleZoom />
-          <PictureWithTextbox/>
-          <BlueBoy />
-          
-        </div>;
-
-  
-}
-// App.js
-import React from 'react';
-import Header from './header';
-import Footer from './footer';
-
-const App = () => {
   return (
     <div>
+      <SimpleZoom />
+      <PictureWithTextbox />
+      <BlueBoy />
       <Header />
-      {/* Add other components and content of your website */}
       <Footer />
     </div>
   );
-};
+}
+// App.js
+// import React from 'react';
+// import Header from './header';
+// import Footer from './footer';
 
-
+// const App = () => {
+//   return (
+//     <div>
+//       <Header />
+//       {/* Add other components and content of your website */}
+//       <Footer />
+//     </div>
+//   );
+// };
 
 export default App;
